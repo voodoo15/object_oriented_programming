9d59c16f901e18d11e134f174b7474443b813c1f
Arnold Redoblado <arnold.redoblado@gmail.com>
2016-03-24 14:34:30 -0400

    Updated Mars rover to include methods for displaying location, setting location and set map boundaries

--- mars_rover.rb
+++ mars_rover.rb
@@ -4,22 +4,44 @@
 #Purpose:  Create a Mars rover and be able to move it around a 10x10 grid with directions
 
 class Rover
-  attr_accessor :name, :coord_x, :coord_y, :direction
+  attr_accessor :name, :coord_x, :coord_y, :direction, :bound_x, :bound_y
 
-  def initialize( in_name, in_x, in_y, in_direct )
+  def initialize( in_name, options = {} )
     @name = in_name
-    @coord_x = :in_x
-    @coord_y = :in_y
+    @coord_x = options[ :coord_x ]
+    @coord_y = options[ :coord_y ]
+    @direction = options[ :direction ]
+    @bound_x = options[ :bound_x ]
+    @bound_y = options [ :bound_y ]
+
+  end
+
+  def set_location( in_x, in_y, in_direct )
+
+    @coord_x = in_x
+    @coord_y = in_y
     @direction = in_direct
 
-  def move( )
-    @coord_x = :coord_x
-    @coord_y = :coord_y
-    @direction = :direction
+  end
 
+  def set_map( inbound_x, inbound_y )
 
+      @bound_x = inbound_x
+      @bound_y = inbound_y
 
+  end
+
+  def show_location
+
+    puts "x coordinate:  #{ @coord_x }"
+    puts "y coordinate:  #{ @coord_y }"
+    puts "direction:  #{ @direction }"
 
   end
 
 end
+
+mars1 = Rover.new( "Mars1", { :coord_x => 1, :coord_y => 2, :direction => "N" } )
+mars1.show_location
+mars2 = Rover.new( "Mars2")
+mars2.set_location( 3, 3, "E" )
