4d8555182dc3dfbec13a176fb8d319ce192e0a87
Arnold Redoblado <arnold.redoblado@gmail.com>
2016-03-24 14:12:52 -0400

    Created Mars rover file and updated initialize

---
+++ mars_rover.rb
@@ -0,0 +1,25 @@
+#Exercise 2
+#Name:  Arnold Redoblado
+#Date:  2016-03-24
+#Purpose:  Create a Mars rover and be able to move it around a 10x10 grid with directions
+
+class Rover
+  attr_accessor :name, :coord_x, :coord_y, :direction
+
+  def initialize( in_name, in_x, in_y, in_direct )
+    @name = in_name
+    @coord_x = :in_x
+    @coord_y = :in_y
+    @direction = in_direct
+
+  def move( )
+    @coord_x = :coord_x
+    @coord_y = :coord_y
+    @direction = :direction
+
+
+
+
+  end
+
+end
