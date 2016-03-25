3b73c98c0b950b802498a2a9e0600734123edbab
Arnold Redoblado <arnold.redoblado@gmail.com>
2016-03-25 10:06:56 -0400

    Committing before I mess around with fixing boundaries again.  Seems everything else is working.

--- mars_rover.rb
+++ mars_rover.rb
@@ -38,13 +38,18 @@ class Rover
     puts "x coordinate:  #{ @coord_x }"
     puts "y coordinate:  #{ @coord_y }"
     puts "direction:  #{ @direction }"
+    puts "x boundary:  #{ @bound_x }"
+    puts "y boundary:  #{ @bound_y }"
 
   end
 
    def move( incoming_instructions )
 
+    #Needed a way to keep track of direction when rotated.  R = +1, L = -1.
+    #Put a condition to go back to index 0 if > 3 (W => N).
     direct_grid = [ "N", "E", "S", "W" ]
 
+    #Split movements if placed as group
      instructions = incoming_instructions.split( // )
 
      instructions.each { | movement |
@@ -56,28 +61,37 @@ class Rover
 
         when "R"
 
-          #If my directions go from west to north > 3 use option one because it will pass end of array
-          if ( direct_grid.index( @direction.to_s ) + 1 ) > 3
-
+          #If my directions go from west to north ( array > 3 ) make north because pointer will pass end of array
+          if ( ( direct_grid.index( @direction.to_s ).to_i + 1 ) > direct_grid.length )
             @direction = "N"
 
           else
-
-            @direction = direct_grid[ ( direct_grid.index( @direction.to_s ) + 1 ) ]
+            @direction = direct_grid[ ( direct_grid.index( @direction.to_s ).to_i + 1 ) ]
 
         end
 
         when "M"
 
-          if @direction == "N"
+          #if @direction == "N"
+          if @direction == "N" #&& ( @coord_y += 1 ).between?( 0, @bound_y.to_i )
               @coord_y += 1
-            elsif @direction == "E"
+
+            elsif @direction == "E" #&& ( @coord_x += 1 ).between?( 0, @bound_x.to_i )
               @coord_x += 1
-            elsif @direction == "S"
+
+            elsif @direction == "S" #&& ( @coord_y -= 1 ).between?( 0, @bound_y.to_i )
               @coord_y -= 1
-            elsif @direction == "W"
+
+            elsif @direction == "W" #&& ( @coord_x -= 1 ).between?( 0, @bound_x.to_i )
               @coord_x -= 1
-          end
+
+          # else
+          #   puts "Instruction either not understood or out of bounds"
+
+        end
+
+        #Keeping in case I need to debug and see what is going on with my coordinates
+        # self.show_location
 
       end
 
@@ -87,13 +101,24 @@ class Rover
 
 end
 
-# mars1 = Rover.new( "Mars1", { :coord_x => 1, :coord_y => 2, :direction => "N" } )
-# mars1.move( "LMLMLMLMM" )
-# mars1.show_location
+#main
 
+#initialize Mars1
+mars1 = Rover.new( "Mars1", { :coord_x => 1, :coord_y => 2, :direction => "N" } )
+#show initial location
+mars1.show_location
+#move my rover via instructions provided
+mars1.move( "LMLMLMLMM" )
+#show final location
+mars1.show_location
 
+#initialize Mars2
 mars2 = Rover.new( "Mars2" )
+#try the other method of setting up initial location
 mars2.set_location( 3, 3, "E" )
+#display initial location
 mars2.show_location
+#move my rover via instructions provided
 mars2.move( "MMRMMRMRRM" )
+#show final location
 mars2.show_location
